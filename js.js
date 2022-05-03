554fddfb4ff79fce8f20c3e64137c04e852c0c6d
Abbott-4 <103395263+Abbott-4@users.noreply.github.com>
2022-05-03 21:07:51 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+    var x;
+
+    x = document.getElementById("myText").value;
+
+   
+    if (x=="student") {
+        window.location.replace("https://abbott-4.github.io/Abbott88/course.html")
+    } else if (x=="parent"){
+        window. location.replace("https://abbott-4.github.io/Abbott88/index.html")
+    
+    }
+    else if (x!="student"){
+         alert("Please enter the correct format!")
+        }
+        else if (x!="parent"){
+             alert("Please enter the correct format!")
+  
+    }
+}
+
+a
